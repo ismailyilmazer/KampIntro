e6836f49c628c3444104c1d647a4dea2af00f924
ismailyilmazer <ismail123yilmazer@gmail.com>
2022-09-02 20:08:02 +0300

    2.09.2022

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList<string> isimler = new MyList<string>();
+
+           
+
+            isimler.Add("bjorn");
+            isimler.Add("ivar");
+
+            isimler.Show();
+
+
+
+            
+           
+
+        }
+    }
+}
